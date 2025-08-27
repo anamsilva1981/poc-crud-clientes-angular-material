92379c6a3526448849f5d9ae428bb5b98efe92f3
anamsilva1981 <ana.msilva@hotmail.com>
2025-08-26 22:48:29 -0300

    feat(cadastro): adiciona importações do angular materia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar'
-import {MatIconModule } from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule ],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule, MatButtonModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
